a6c2d769fa0e78f1a41c1305fccf17204bc856fc
beizhengren <291087211@.com>
2020-06-28 19:45:48 +0800

    92.cpp

---
+++ 5.List/83.RemoveDuplicatesFromSortedList.Easy/83.cpp
@@ -0,0 +1,21 @@
+/* struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+///solution 1
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode* cur = head;
+        while (cur) {
+            while (cur->next && cur->val == cur->next->val)//适用于多个重复元素
+                cur->next = cur->next->next;
+            cur = cur->next;
+        }
+        return head;
+    }
+};
