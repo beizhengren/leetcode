a7047c93e3f40988e3805e95808f47c2f463b175
beizhengren <291087211@qq.com>
2020-09-12 23:33:43 +0800

    Create 55.二叉树的深度.cpp

---
+++ 剑指offer/55.二叉树的深度.cpp
@@ -0,0 +1,19 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if (pRoot == nullptr) return 0;
+        if (pRoot->left == nullptr && pRoot->right == nullptr) return 1;
+        return 1 + max(TreeDepth(pRoot->left), TreeDepth(pRoot->right));   
+    }
+    
+};
