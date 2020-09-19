67e8268e62b6a5a45f1221d345cb8f9224c32cd1
beizhengren <291087211@qq.com>
2020-09-19 13:34:59 +0800

    Update 13.机器人的运动范围.cpp

--- 剑指offer/13.机器人的运动范围.cpp
+++ 剑指offer/13.机器人的运动范围.cpp
@@ -14,7 +14,7 @@ public:
     
 private:
     int helper(int k, int rows, int cols, int strRow, int strCol){
-        if( strRow <0 || strRow >= rows || strCol <0 || strCol >= cols) return 0;
+        if( strRow <0 || strRow >= rows || strCol <0 || strCol >= cols) return 0;//一定要排除下标<0的情况
         int Num = 0;
         int idx = strRow * cols + strCol;
         if(getSum(strRow, strCol) <= k && memo[idx] == false){
@@ -43,5 +43,60 @@ private:
         }
         sum = sum1 + sum2;
         return sum;
+    }
+};
+
+
+
+class Solution {
+public:
+    int movingCount(int threshold, int rows, int cols)
+    {
+        //1.sepcial case
+        if(threshold < 0 || rows < 1 || cols < 1)
+            return 0;
+        //2.general case
+        for(int i = 0; i < rows * cols; ++i)
+            memo.emplace_back(false);
+        
+        int res = 0;
+        helper(threshold, rows, cols, 0, 0, res);
+        return res;
+    }
+    
+private:
+    void helper(int threshold, int rows, int cols, int strRow, int strCol, int& res){
+        if(strCol >= cols || strRow >= rows|| strCol<0 || strRow <0) return;
+        int index = strRow * cols + strCol;
+        if(!memo[index]){
+            memo[index] = true;
+            if(isMeetCond(strRow, strCol, threshold)){
+                  res++;
+                for(int i = 0; i < 4; ++i){
+                    int newRow = strRow + dir[i][0];
+                    int newCol = strCol + dir[i][1];
+                    helper(threshold, rows, cols, newRow, newCol, res);
+                }
+            }
+        }
+    }
+    
+    vector<bool> memo;
+    int dir[4][2] = {{0,1}, {1,0}, { -1, 0}, {0, -1}};
+    bool isMeetCond(int row, int col, int threshold){
+        int sumRow = 0, sumCol = 0, sum = 0;
+        while(row != 0){
+            sumRow += row%10;
+            row /= 10;
+        }
+        while(col != 0){
+            sumCol += col%10;
+            col /= 10;
+        }
+        sum = sumRow + sumCol;
+        if(sum <= threshold)
+            return true;
+        else 
+            return false;
     }
 };
