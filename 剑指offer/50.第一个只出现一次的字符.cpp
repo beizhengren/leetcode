ef85c2bff959b642ede6bbe641fbe601f2c4714e
beizhengren <291087211@qq.com>
2020-09-19 17:04:33 +0800

    Update 50.第一个只出现一次的字符.cpp

--- 剑指offer/50.第一个只出现一次的字符.cpp
+++ 剑指offer/50.第一个只出现一次的字符.cpp
@@ -1,9 +1,10 @@
 //哈希
+//给的字符串是按字母顺序的，因此从小到大依次扫描，就是和字符串中的顺序相同。
 class Solution {
 public:
     int FirstNotRepeatingChar(string str) {
         if(str.empty()) return -1;
-        char map[256]{0};//局部变量初始化，key is c, value is times;
+        unsigned int map[256]{0};//局部变量初始化，key is c, value is times[0~10000];
         for(int i = 0; i < str.size(); ++i){
             map[str[i]]++;
         }
