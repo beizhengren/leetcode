61997e8660f9817206368658e37b24949e2a52c9
beizhengren <291087211@qq.com>
2020-11-17 19:03:58 +0800

    Create quick_sort.hpp

---
+++ 0.sort/quick_sort/quick_sort.hpp
@@ -0,0 +1,15 @@
+//
+// Created by wyz on 20-11-17.
+//
+
+#ifndef PROJECT_QUICKSORT_HPP
+#define PROJECT_QUICKSORT_HPP
+
+void quick_sort_v1(int*, int, int);
+void quick_sort_v1_opt(int*, int, int);
+
+void quick_sort_vn(int*, int, int);
+void quick_sort_vn_opt(int* arr, int l, int r);
+
+void bubble_sort(int*, int);
+#endif //PROJECT_QUICKSORT_HPP
