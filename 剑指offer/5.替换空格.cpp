69e43d6c22fef62fe109a20cf65f9bbddf5fc9c4
beizhengren <291087211@qq.com>
2020-12-02 22:04:39 +0800

    Update 5.替换空格.cpp

--- 剑指offer/5.替换空格.cpp
+++ 剑指offer/5.替换空格.cpp
@@ -27,3 +27,36 @@ public:
         return;
 	}
 };
+
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        size_t oldSize = s.size();
+        size_t count = 0;
+        for (int i = 0; i < oldSize; ++i) {
+            // 注意条件是“==”
+            if(s[i] == ' '){
+                ++count;
+            }
+        }
+        //“多“了 2 * count个
+        s.resize(oldSize + 2 * count);
+        size_t newSize = s.size();
+        // 如果 i == j，跳出循环，后面的不用再遍历
+        for (int i = newSize - 1, j = oldSize - 1; i > j; ) {
+            if(s[j] == ' '){
+                s[i] = '0';
+                s[i-1] = '2';
+                s[i-2] = '%';
+                i-=3;
+                --j;
+            }else{
+                s[i] = s[j];
+                --i;
+                --j;
+            }
+        }
+        return s;
+    }
+};
