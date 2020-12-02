11c7ff8dd2296d3fd9bf0d087aadcb59b9496705
beizhengren <291087211@qq.com>
2020-12-02 23:58:10 +0800

    Update 151. 翻转字符串里的单词.cpp

--- 字符串/151. 翻转字符串里的单词.cpp
+++ 字符串/151. 翻转字符串里的单词.cpp
@@ -45,7 +45,7 @@ private:
             if (fastIndex - 1 > 0
                     && s[fastIndex - 1] == s[fastIndex]
                     && s[fastIndex] == ' ') {
-                continue;
+                continue;自己
             } else {
                 s[slowIndex++] = s[fastIndex];
             }
@@ -60,3 +60,71 @@ private:
         }
     }
 };
+
+
+//自己
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int slowIdx = 0;
+        int fastIdx = 0;
+        //1.消除前面空格, 如何保存去除之后的字符串。
+        for (; fastIdx < s.size(); ++fastIdx) {
+            if (s[fastIdx] == ' ') { continue; }
+            else {break;}
+        }
+        //此时fastIdx 已经指向第一个不为 ' '的元素
+
+        //2.去除中间重复空格 
+        for(; fastIdx < s.size(); ++fastIdx) {
+            // 后边用到了fastIdx - 1做下标
+            if ( fastIdx - 1 > 0
+             && s[fastIdx] == s[fastIdx - 1]
+             && s[fastIdx] == ' ') {
+                continue;
+            }
+            else {
+                s[slowIdx] = s[fastIdx];
+                ++slowIdx;
+            }
+        }
+        //3.消除后面空格
+        //上面操作完成之后有可能 slowIdx-1这个位置是空格
+        //也有可能不是.
+        //带下标运算的一定要检查边界，当前位置最小是0
+        if(slowIdx - 1 > 0 && s[slowIdx-1] == ' ') { s.resize(slowIdx - 1);}
+        else { s.resize(slowIdx);}//resize(个数)
+        //4.反转单词
+        reverseOneWord(s, 0, s.size()- 1);
+
+        fastIdx = 0;
+        slowIdx = 0;
+        for( ; fastIdx < s.size(); ++fastIdx){
+            if(s[fastIdx] == ' ') { 
+                reverseOneWord(s, slowIdx, fastIdx - 1);
+                slowIdx = fastIdx + 1;
+            }else {
+                continue;
+            }
+        }
+        // 注意 跳出条件的时候是 fastIdx到达了字符串末尾
+        // 最后一个空格之后的字符， 到末尾字符构成的字符串
+        // 还没有进行反转
+        reverseOneWord(s, slowIdx, fastIdx - 1);
+        return s;
+    }
+
+private:
+    void reverseOneWord(string& ss, int l, int r) {
+        while(l < r){
+            swap(ss[l], ss[r]);
+            //注意变量的更新
+            ++l;
+            --r;
+        }
+    }
+};
+
+
+
