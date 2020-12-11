aaf53962ed9029b7262f2104380f404409612ba5
beizhengren <291087211@qq.com>
2020-12-11 19:16:15 +0800

    Update 93. 复原IP地址.cpp

--- 回溯算法/93. 复原IP地址.cpp
+++ 回溯算法/93. 复原IP地址.cpp
@@ -1,4 +1,59 @@
-//Solution
+// Solution 1
+class Solution {
+private:
+    string path;
+    vector<string> result;// 记录结果
+    // startIndex: 搜索的起始位置，pointNum:添加逗点的数量
+    void backtracking(string& s, int startIndex, int pointNum) {
+        //"点号" 大于4不满足题意
+        if (pointNum > 4) { return;}
+        if (pointNum == 4 && startIndex == s.size()) { // 逗点数量为3时，分隔结束
+            //pop掉“点号”
+            path.pop_back();
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < s.size(); i++) {
+            if (isValid(s, startIndex, i)) { // 判断 [startIndex,i] 这个区间的子串是否合法
+                int size = path.size();
+                path.append(s, startIndex, i - startIndex + 1);
+                path.push_back('.');
+                pointNum++;
+                backtracking(s, i + 1, pointNum);   // 插入逗点之后下一个子串的起始位置为i+2
+                pointNum--;                         // 回溯
+                path.resize(size);
+            } else break; // 不合法，直接结束本层循环
+        }
+    }
+    // 判断字符串s在左闭又闭区间[start, end]所组成的数字是否合法
+    bool isValid(const string& s, int start, int end) {
+        if (start > end) {
+            return false;
+        }
+        if (s[start] == '0' && start != end) { // 0开头的数字不合法
+                return false;
+        }
+        int num = 0;
+        for (int i = start; i <= end; i++) {
+            if (s[i] > '9' || s[i] < '0') { // 遇到非数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s[i] - '0');
+            if (num > 255) { // 如果大于255了不合法
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    vector<string> restoreIpAddresses(string s) {
+        result.clear();
+        backtracking(s, 0, 0);
+        return result;
+    }
+};
+
+//Solution 2
 class Solution {
 private:
     string path;
