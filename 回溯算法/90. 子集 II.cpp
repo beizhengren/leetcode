8b0b86306e2c6424bbfd97b694f4d26db051e16d
beizhengren <291087211@qq.com>
2020-12-13 16:07:43 +0800

    Create 90. 子集 II.cpp

---
+++ 回溯算法/90. 子集 II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        // 去重必须排序！！！
+        sort(nums.begin(), nums.end());
+        backtracking(nums, 0, used);
+        return result;
+    }
+private:
+    void backtracking(const vector<int>& nums, int startIndex, vector<bool>& used) {
+        result.push_back(path);
+        
+        if (startIndex >= nums.size()) {return;}
+        for (int i = startIndex; i < nums.size(); ++i) {
+            // used[i - 1] == true，说明同一树支candidates[i - 1]使用过
+            // used[i - 1] == false，说明同一树层candidates[i - 1]使用过
+            // 而我们要对同一树层使用过的元素进行跳过
+            if(i > 0 && used[i-1] == false && nums[i-1] == nums[i]) {continue;}
+
+            if (used[i] == false) {
+                used[i] = true;
+                path.push_back(nums[i]);
+                backtracking(nums, i + 1, used);
+                path.pop_back();
+                used[i] = false;
+            }
+        }
+    }
+
+    vector<vector<int>> result;
+    vector<int>path;
+};
