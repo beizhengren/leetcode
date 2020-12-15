43fc764e25acfe5e3c88ba82bf1396d408079474
beizhengren <291087211@qq.com>
2020-12-15 11:42:01 +0800

    Update 53. 最大子序和.cpp

--- 贪心算法/53. 最大子序和.cpp
+++ 贪心算法/53. 最大子序和.cpp
@@ -20,3 +20,31 @@ public:
         return result;
     }
 };
+
+//Solution 2
+//贪心算法
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // result初始值为最小
+        int result = std::numeric_limits<int>::min();
+        // 当前的序列和
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            //1. 由于从i = 0开始，那么进来就要加nums[i]，这样才是第一个sum的初始值
+            sum += nums[i];
+            //2. 与result作比较，更新当前sum。
+            // 此步一定在3之前，因为2要先用完当前sum（当前sum以及result都有可能是负的）
+            // 3是根据当前的sum判断下一次是否sum初始化成0,
+            if (sum > result) {
+                result = sum;
+            } 
+            //3. 如果当前sum的值<=0，那么下一个i，就要从新计算sum，因此恢复sum = 0
+            if (sum <= 0) {
+                sum = 0;
+            }
+        }
+        
+        return result;
+    }
+};
