1dfb90b4d57f4980c7434b4fdbb28d908892d230
beizhengren <291087211@qq.com>
2020-12-16 22:42:48 +0800

    Update 1.二叉树非递归遍历.cpp

--- 二叉树/1.二叉树非递归遍历.cpp
+++ 二叉树/1.二叉树非递归遍历.cpp
@@ -22,36 +22,32 @@ public:
 
 	void pre0rder(TreeNode* cur) {
 		if (cur == nullptr) { return; }
-    // 构造一个栈， 用来存储 树的节点指针
+		// 构造一个栈， 用来存储 树的节点指针
 		stack<TreeNode*> stk;
 		stk.push(cur);
 		//m l r
 		while (!stk.empty()) {
-      // 每次判断栈顶的节点指针
+      			// 每次判断栈顶的节点指针
 			if (stk.top()) {
-        //取出栈顶节点指针，暂存
+        			//取出栈顶节点指针，暂存
 				auto node = stk.top();
 				stk.pop();
 				
-        // 按照遍历的逆序，压入栈中
+        			// 按照遍历的逆序，压入栈中
 				// right
-				if (node->right != nullptr) {
-					stk.push(node->right);
-				}
+				if (node->right != nullptr) {stk.push(node->right);}
 				// left
-				if (node->left != nullptr) {
-					stk.push(node->left);
-				}
+				if (node->left != nullptr) {stk.push(node->left);}
 				// mid
-        //每次push完当前节点，都需要push个nullptr作为标记
-        //移动这两行代码的顺序，可以构造出不同的遍历方式。
+        			//每次push完当前节点，都需要push个nullptr作为标记
+        			//移动这两行代码的顺序，可以构造出不同的遍历方式。
 				stk.push(node);
 				stk.push(nullptr);
 			}
 			else {
-        // 弹出 nullptr
+        			// 弹出 nullptr
 				stk.pop();
-        // 取栈顶指针
+        			// 取栈顶指针
 				auto node = stk.top();
 				res.push_back(node->val);
 				stk.pop();
