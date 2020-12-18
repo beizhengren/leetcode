1f559d34fbe0b39d2b6bfa654bc98e9101e30b60
beizhengren <291087211@qq.com>
2020-12-18 09:03:16 +0800

    Update 226. 翻转二叉树.cpp

--- 二叉树/226. 翻转二叉树.cpp
+++ 二叉树/226. 翻转二叉树.cpp
@@ -1,3 +1,5 @@
+// Solution 1
+// DFS
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -12,6 +14,38 @@ public:
         
         // 3.翻转以左孩子为根的 右子树
         invertTree(root->right);
+        return root;
+    }
+};
+
+// Solution 2
+// DFS 迭代法
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {return root;}
+        stack<TreeNode*> stk;
+        stk.push(root);
+
+        while (!stk.empty()) {
+            if (stk.top()) {
+                TreeNode* node = stk.top();
+                stk.pop();
+                // pre: m ->l -> r
+                // 注意：不管你以什么顺序访问这棵说，都要交换左右孩子
+                // 而 注释掉 swap && 将pre换成 m ->r ->l是错误的
+                swap(node->right, node->left);
+                if (node->right) {stk.push(node->right);}
+                if (node->left) {stk.push(node->left);}
+                stk.push(node);
+                stk.push(nullptr);
+
+            } else {
+                stk.pop();
+                TreeNode* node = stk.top();
+                stk.pop();
+            }
+        }
         return root;
     }
 };
