9a93827480303f646f3c56c051242f11d5bb487b
beizhengren <291087211@qq.com>
2020-12-18 09:09:16 +0800

    Update 226. 翻转二叉树.cpp

--- 二叉树/226. 翻转二叉树.cpp
+++ 二叉树/226. 翻转二叉树.cpp
@@ -49,3 +49,26 @@ public:
         return root;
     }
 };
+
+// Solution 3
+// BFS迭代法
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {return root;}
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = que.front();
+                que.pop();
+
+                swap(node->left, node->right);
+                if (node->left) {que.push(node->left);}
+                if (node->right){que.push(node->right);}
+            }
+        }
+        return root;
+    }
+};
