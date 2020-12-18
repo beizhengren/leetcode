2502efd1e35ad40c64a832ea97c3ffc56be63558
beizhengren <291087211@qq.com>
2020-12-18 10:58:19 +0800

    Update 226. 翻转二叉树.cpp

--- 二叉树/226. 翻转二叉树.cpp
+++ 二叉树/226. 翻转二叉树.cpp
@@ -1,5 +1,6 @@
 // Solution 1
 // DFS
+// 前序和后序都可以，中序在递归的方法不可以。因为在递归到root，交换左右孩子后会出现问题，详见Solution2解释。
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -19,7 +20,8 @@ public:
 };
 
 // Solution 2
-// DFS 迭代法
+// DFS 迭代法（3种方法都能够实现。因为每次在stack中取指针，这些都是事先存好的。
+// 即便中序遍历到树根，将树根左右孩子交换，然后再取pop的时候还是之前的”右孩子“，而不是交换后的右孩子（”也就是交换之前的左孩子“））
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -52,6 +54,36 @@ public:
     }
 };
 
+// Solution 2.1
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {return root;}
+        stack<TreeNode*> stk;
+        stk.push(root);
+
+        while (!stk.empty()) {
+            if (stk.top()) {
+                TreeNode* node = stk.top();
+                stk.pop();
+                // pre: m ->l -> r
+                // post: l -> r -> m
+                // mid: l -> mid -> r 
+                if (node->right) {stk.push(node->right);}
+                stk.push(node);
+                stk.push(nullptr);
+                if (node->left) {stk.push(node->left);}
+
+            } else {
+                stk.pop();
+                TreeNode* node = stk.top();
+                swap(node->right, node->left); //节点处理逻辑
+                stk.pop();
+            }
+        }
+        return root;
+    }
+};
 // Solution 3
 // BFS迭代法
 class Solution {
