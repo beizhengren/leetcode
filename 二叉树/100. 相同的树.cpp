633e78d1ecae260d9083aeccfc975d045f984239
beizhengren <291087211@qq.com>
2020-12-19 19:11:02 +0800

    Update 100. 相同的树.cpp

--- 二叉树/100. 相同的树.cpp
+++ 二叉树/100. 相同的树.cpp
@@ -12,3 +12,37 @@ public:
         }
     }
 };
+
+// Solution 2
+// 迭代 队列 成对儿入队， 什么序都不是， 只是用队列保存数据
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {return true;}
+        queue<TreeNode*> que;
+        que.push(p);
+        que.push(q);
+        while (!que.empty()) {
+            TreeNode* nodeP = que.front(); que.pop();
+            TreeNode* nodeQ = que.front(); que.pop();
+            if (nodeP == nullptr && nodeQ == nullptr) {
+                continue;
+            }
+            if (nodeP != nullptr && nodeQ == nullptr) {
+                return false;
+            }
+            if (nodeP == nullptr && nodeQ != nullptr) {
+                return false;
+            }
+            if (nodeP->val != nodeQ->val) {
+                return false;
+            } else {
+                que.push(nodeP->left);
+                que.push(nodeQ->left);
+                que.push(nodeP->right);
+                que.push(nodeQ->right);
+            }
+        }
+        return true;
+    }
+};
