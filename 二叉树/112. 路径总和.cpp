18e889d37d26039ecd5b0a0e4afd941650d1df73
beizhengren <291087211@qq.com>
2020-12-20 21:10:17 +0800

    Update 112. 路径总和.cpp

--- 二叉树/112. 路径总和.cpp
+++ 二叉树/112. 路径总和.cpp
@@ -45,3 +45,52 @@ private:
         return false;
     } 
 };
+
+// Solution 3
+// 迭代 前序
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == nullptr) {return false;}
+        stack<TreeNode*> stk;
+        stk.push(root);
+        stack<int> stkSum;
+        stkSum.push(root->val);
+        while (!stk.empty()) {
+            if (stk.top()) {
+                TreeNode* node  = stk.top();
+                stk.pop();
+                int value = stkSum.top();
+                stkSum.pop();
+                // pre: m l r
+                if (node->right) {
+                    // 保存当前节点的状态（路径和）
+                    stkSum.push(value + node->right->val);
+                    stk.push(node->right);
+                }
+
+                if (node->left) {
+                    stkSum.push(value + node->left->val);
+                    stk.push(node->left);
+                }
+                stk.push(node);
+                stk.push(nullptr);
+                stkSum.push(value);
+            } else {
+                stk.pop();
+                TreeNode* node = stk.top();
+                stk.pop();
+                int value = stkSum.top();
+                stkSum.pop();
+                if (node != nullptr 
+                    && node->left == nullptr
+                    && node->right == nullptr) {
+                        if (value == sum) {
+                            return true;
+                        }
+                }
+            }
+        }
+        return false;
+    }
+};
