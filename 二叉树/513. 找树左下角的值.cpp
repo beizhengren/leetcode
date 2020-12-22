0c664ce3d5c329ab69e82fbd76948e4beb819f48
beizhengren <291087211@qq.com>
2020-12-22 12:22:01 +0800

    Update 513. 找树左下角的值.cpp

--- 二叉树/513. 找树左下角的值.cpp
+++ 二叉树/513. 找树左下角的值.cpp
@@ -56,3 +56,35 @@ private:
     int result;
     int maxLevel;
 };
+
+// Solution 3
+// DFS 前序
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        findBottomLeftValue(root, 0);
+        return result;
+    }
+private:
+    void findBottomLeftValue(TreeNode* node, int curLevel) {
+        if (node == nullptr) {return;}
+        // 中
+        if (curLevel > maxLevel) {
+            result = node->val;
+            maxLevel = curLevel;//只在第一次(左子树)更新maxLevel
+        }
+        // 左
+        // 注意!!!这里必须是curLevel + 1, 不能用maxLevel
+        // 因为先递归左子树,更新maxLevel. 等递归右子树的时候有可能curLevel < maxLevel
+        if (node->left) {
+            findBottomLeftValue(node->left, curLevel + 1);
+        }
+        //右
+        // 必须遍历右子树, 因为最大层(结果)有可能在右子树里
+        if (node->right) {
+            findBottomLeftValue(node->right, curLevel + 1);
+        }
+    }
+    int result = 0;
+    int maxLevel = INT_MIN;
+};
