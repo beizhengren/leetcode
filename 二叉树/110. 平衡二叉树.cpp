d3e58091d4610dcd9edc7dbc26d6ba38cd17fd87
beizhengren <291087211@qq.com>
2020-12-22 11:42:57 +0800

    Update 110. 平衡二叉树.cpp

--- 二叉树/110. 平衡二叉树.cpp
+++ 二叉树/110. 平衡二叉树.cpp
@@ -1,5 +1,7 @@
 // 二叉树节点的深度：指从根节点到该节点的最长简单路径边的条数。 从上到下去查 所以需要前序遍历（中左右）
 // 二叉树节点的高度：指从该节点到叶子节点的最长简单路径边的条数。 从下到上去查，所以只能后序遍历（左右中）
+
+// Solution 1
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
@@ -20,5 +22,26 @@ private:
         if (abs(heightL - heightR) >= 2) {return -1;}
         //平衡，就返回当前树的高度
         return 1 + max(heightL, heightR);
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) {return true;}
+        // 左子树最大深度
+        int maxL = getMaxLen(root->left);
+        // 右子树最大深度
+        int maxR = getMaxLen(root->right);
+        // 如果左右子树深度 > 1,不满足条件 返回false
+        if (maxL - maxR > 1 || maxR - maxL > 1) {return false;}
+        // 如果左右子树深度 <= 1, 那么就要递归判断左右子树是不是满足条件
+        else {return isBalanced(root->left) && isBalanced(root->right);}
+    }
+    // 求当前以node为根的二叉树的最大深度
+    int getMaxLen (TreeNode* node) {
+        if (node == nullptr) {return 0;}
+        return 1 + max (getMaxLen(node->left), getMaxLen(node->right));
     }
 };
